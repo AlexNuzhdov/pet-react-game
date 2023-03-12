246c5762abc1d8dde1d60ff81d79b95cc697bbba
AlexNuzhdov <100243457+AlexNuzhdov@users.noreply.github.com>
2023-03-12 21:16:35 +0200

    add json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="pet-react-game">
       <App />
     </BrowserRouter>
   </React.StrictMode>
